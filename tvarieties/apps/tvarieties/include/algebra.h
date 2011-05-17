42a41edbfd2883a4a3163797239b56f64bff4dfe
Lars Kastner <lkastner@mi.fu-berlin.de>
2011-05-17 14:42:40 +0200

    Next try as extension of Array<Polynomial<> >.

--- tvarieties/apps/tvarieties/include/algebra.h
+++ tvarieties/apps/tvarieties/include/algebra.h
@@ -6,39 +6,11 @@
 
 namespace polymake { namespace tvarieties {
 
-class Ideal {
-	Array<Polynomial<> > gens;
+class Ideal : Array<Polynomial<> > {
 public:
 	Ideal() {}
 
 	~Ideal() {}
-
-Ideal(const Polynomial<>& p) : gens()
-	{
-		gens.resize(1);
-		gens[0] = p;
-	}
-
-	Polynomial<>& operator() (int i) 
-	{
-		return gens[i];
-	}
-	const Polynomial<>& operator() (int i) const
-	{
-		return gens[i];
-	}
-
-template <typename Output> friend
-Output& operator<< (GenericOutput<Output>& out, const Ideal& me)
-	{
-		for(Array<Polynomial<> >::const_iterator it = me.gens.begin(); it != me.gens.end(); ++it) {
-			out.top() << *it << "\n";
-		}
-		out.top() << "number of gens: ";
-		out.top() << me.gens.size();
-		return out.top();
-	}
-
 };
 
 // possibly do the following here:
