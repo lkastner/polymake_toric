fe76120541bdfb44d693005fa8c66650312dd6f8
Lars Kastner <lars.kastner@yahoo.dk>
2012-02-16 23:40:17 +0100

    Fixed RELEVANT_CONES.

--- scripts/hyperdet.cmd
+++ scripts/hyperdet.cmd
@@ -7,6 +7,7 @@ $g = $x000*$x000*$x111*$x111 + $x001*$x001*$x110*$x110 + $x010*$x010*$x101*$x101
 $g = $g - 2*($x000*$x001*$x110*$x111 + $x000*$x010*$x101*$x111 + $x000*$x100*$x011*$x111 + $x001*$x010*$x101*$x110 + $x001*$x100*$x011*$x110 + $x010*$x100*$x011*$x101);
 $g = $g + 4*($x000*$x011*$x101*$x110 + $x001*$x010*$x100*$x111);
 $htv = chow_quotient($g);
+#$htv = load("hyperdet.htv");
 
 $amb = $htv->AMBIENT_TV;
 $rel = $htv->RELEVANT_CONES;
@@ -28,6 +29,7 @@ $ambmc = $amb->MAXIMAL_CONES;
 @ambmc = @$ambmc;
 $cs = (new Set<Int>([0..($amb->RAYS->rows-1)]))-$s;
 print $cs;
+@relc = map((new Cone<Rational>(RAYS=>$amb->RAYS->minor($_, All))), @relc);
 @non_bad = grep(($cs * $_)->size == 0, @ambmc);
 print @non_bad;
 foreach my $t (@$cs){
@@ -36,11 +38,16 @@ foreach my $t (@$cs){
 	map($nc += (new Set<Int>($_)), @cont);
 	$nc -= $cs;
 	$nc -= new Set<Int>($t);
-	print $nc;
+	print $nc, "\n";
+	$new_cone = new Cone<Rational>(INPUT_RAYS=>$amb->RAYS->minor($nc, All));
+	print $new_cone->RAYS->rows," ", $new_cone->INPUT_RAYS->rows, "\n";
+	my @check = map(included_polyhedra($_,$new_cone), @relc);
+	print @check, "\n";
 	push @non_bad, $nc;
 	print @cont, "\n\n";
+	
 }
-
+$f = new PolyhedralFan<Rational>(INPUT_RAYS=>$amb->RAYS, MAXIMAL_CONE=>\@non_bad);
 #$p = load("cool.poly");
 #$minkp = new Matrix<Rational>($p->MINKOWSKI_SUMMAND_CONE->EQUATIONS);
 #$eqs = new Matrix<Rational>($p->GRAPH->EDGE_DIRECTIONS);
