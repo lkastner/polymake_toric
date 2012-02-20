7e54cd0e682836f4bf51e0d4f486904479c110de
Anna-Lena Winz <anna-lena@medusa.(none)>
2012-02-20 13:14:49 +0100

    Routine commit from Medusa.

--- apps/common/src/perl/auto-polynomials.cc
+++ apps/common/src/perl/auto-polynomials.cc
@@ -21,12 +21,5 @@
 #include "polymake/Rational.h"
 namespace polymake { namespace common {
 ///==== Automatically generated contents follow.    Please do not delete this line. ====
-   template <typename T0, typename T1>
-   FunctionInterface4perl( polynomials_X_f1, T0,T1 ) {
-      perl::Value arg0(stack[0]), arg1(stack[1]);
-      WrapperReturn( arg0.get<T0>().polynomials(arg1.get<T1>()) );
-   };
-
-   FunctionInstance4perl(polynomials_X_f1, perl::Canned< const SingularIdeal >, perl::Canned< const Ring< Rational, int > >);
 ///==== Automatically generated contents end here.  Please do not delete this line. ====
 } }
