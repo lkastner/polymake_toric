dca0fcb5e40b99c2214d45d05af269a9ce850a7a
Anna-Lena Winz <anna-lena@medusa.(none)>
2012-02-20 13:14:49 +0100

    Routine commit from Medusa.

--- algebra/apps/common/src/perl/auto-std.cc
+++ algebra/apps/common/src/perl/auto-std.cc
@@ -21,12 +21,5 @@
 #include "polymake/Rational.h"
 namespace polymake { namespace common {
 ///==== Automatically generated contents follow.    Please do not delete this line. ====
-   template <typename T0, typename T1>
-   FunctionInterface4perl( std_X_f17, T0,T1 ) {
-      perl::Value arg0(stack[0]), arg1(stack[1]);
-      WrapperReturnVoid( arg0.get<T0>().std(arg1.get<T1>()) );
-   };
-
-   FunctionInstance4perl(std_X_f17, perl::Canned< const SingularIdeal >, perl::Canned< const Ring< Rational, int > >);
 ///==== Automatically generated contents end here.  Please do not delete this line. ====
 } }
