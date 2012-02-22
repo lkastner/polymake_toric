e7c66b08d60167813ecfe98134e7575c8b8ed604
Anna-Lena Winz <anna-lena@medusa.(none)>
2012-02-22 16:16:04 +0100

    Routine commit from Medusa.

--- scripts/divisor_test.cmd
+++ scripts/divisor_test.cmd
@@ -1,4 +1,16 @@
 application "fan";
-$v = new Vector(3,4,6);
-$D = new TDivisor(COEFFICIENTS=>$v);
+$f = normal_fan(cube(2));
+$t = cast<ToricVariety>($f);
+$v = new Vector(3,4,6,5);
+$w = new Vector(4.2,5,6,3);
+$z = $f->RAYS->col(1);
+$D = $t->add("DIVISOR",COEFFICIENTS=>$v);
+$D2 = $t->add("DIVISOR",COEFFICIENTS=>$w);
+$D3 = $t->add("DIVISOR",COEFFICIENTS=>$z);
 print $D->INTEGRAL;
+print $D2->INTEGRAL;
+print $D3->INTEGRAL;
+print $D->PRINCIPAL;
+print $D2->PRINCIPAL;
+print $D3->PRINCIPAL;
+
