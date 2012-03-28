fe4ac9aaf3f77d430b138712f81bcadb275b99a7
Lars Kastner <lars.kastner@yahoo.dk>
2012-03-28 15:35:26 +0200

    Trying to eliminate the left_companion and torsion.

--- apps/common/include/Hermite_normal_form.h
+++ apps/common/include/Hermite_normal_form.h
@@ -15,8 +15,8 @@
    $Project: polymake $$Id: Hermite_normal_form.h 10664 2012-03-20 23:14:04Z gawrilow $
 */
 
-#ifndef POLYMAKE_SMITH_NORMAL_FORM_H
-#define POLYMAKE_SMITH_NORMAL_FORM_H
+#ifndef POLYMAKE_HERMITE_NORMAL_FORM_H
+#define POLYMAKE_HERMITE_NORMAL_FORM_H
 
 #include "polymake/SparseMatrix.h"
 #include "polymake/Bitset.h"
@@ -26,16 +26,12 @@
 #include "polymake/list"
 #include "polymake/GenericStruct.h"
 
-namespace pm {
+namespace pm { namespace common {
 
 class dummy_companion_logger {
 public:
    template <typename E>
    void from_right(const SparseMatrix2x2<E>&) const {}
-   template <typename E>
-   void from_left(const SparseMatrix2x2<E>&) const {}
-   template <typename Container>
-   void permute_rows(const Container&) const {}
    template <typename Container>
    void permute_cols(const Container&) const {}
 };
@@ -46,8 +42,6 @@ protected:
    TransposedLogger();
    ~TransposedLogger();
 public:
-   template <typename E>
-   void from_right(const SparseMatrix2x2<E>& U) const { Logger::from_left(T(U)); }
    template <typename E>
    void from_left(const SparseMatrix2x2<E>& U) const { Logger::from_right(T(U)); }
 };
@@ -84,27 +78,13 @@ public:
                         : SparseMatrix2x2<E>(U.i, U.j, -U.a_jj, U.a_ji, U.a_ij, -U.a_ii);
    }
 public:
-   HNF_companion_logger(SparseMatrix<E> *Larg, SparseMatrix<E> *Rarg) : L(Larg), R(Rarg) {}
+   HNF_companion_logger(SparseMatrix<E> *Rarg) : R(Rarg) {}
 
    template <typename Matrix>
    void from_right(const Matrix& U) const
    {
       if (inverse_companions) R->multiply_from_left(inv(U));
       else R->multiply_from_right(U);
-   }
-
-   template <typename Matrix>
-   void from_left(const Matrix& U) const
-   {
-      if (inverse_companions) L->multiply_from_right(inv(U));
-      else L->multiply_from_left(U);
-   }
-
-   template <typename Container>
-   void permute_rows(const Container& perm) const
-   {
-      if (inverse_companions) L->permute_cols(entire(perm));
-      else L->permute_rows(entire(perm));
    }
 
    template <typename Container>
@@ -168,9 +148,9 @@ int hermite_normal_form_steps(Matrix& M, CompanionLogger& Logger
    return start_col;
 }
 
-template <typename E, typename CompanionLogger, bool strict_diagonal>
-int hermite_normal_form(SparseMatrix<E>& M, std::list< std::pair<E,int> >& torsion,
-                      const CompanionLogger& Logger, bool2type<strict_diagonal>)
+template <typename E, typename CompanionLogger>
+int hermite_normal_form(SparseMatrix<E>& M,
+                      const CompanionLogger& Logger)
 {
    const bool Logger_dummy=pm::derived_from<CompanionLogger, dummy_companion_logger>::value;
 
@@ -191,187 +171,24 @@ int hermite_normal_form(SparseMatrix<E>& M, std::list< std::pair<E,int> >& torsi
           hermite_normal_form_steps(T(M), transpose_logger(Logger)) < M.cols()) ;
 #endif
    int rank=0;
-   torsion.clear();
    Array<int> r_perm(strict_diagonal ? M.rows() : 0),
               c_perm(strict_diagonal ? M.cols() : 0);
    Array<int>::iterator rp=r_perm.begin(), rpe=r_perm.end(),
                         cp=c_perm.begin(), cpe=c_perm.end();
 
-   for (typename Entire< Rows< SparseMatrix<E> > >::iterator r=entire(rows(M)); !r.at_end(); ++r) {
-      if (!r->empty()) {
-         ++rank;
-         typename SparseMatrix<E>::row_type::iterator e=r->begin();
-         if (abs_equal(*e,1)) {
-            if (strict_diagonal) *rp++=r.index(), *cp++=e.index();
-         } else {
-            torsion.push_back(std::make_pair(abs(*e), e.index()));
-         }
-      }
-   }
-
-   for (typename std::list< std::pair<E,int> >::iterator t=torsion.begin(), t_end=torsion.end();
-        t != t_end; ++t) {
-      typename std::list< std::pair<E,int> >::iterator t2=t;
-      for (++t2; t2 != t_end; ) {
-         ExtGCD<E> x = ext_gcd(t->first, t2->first);
-         if (t->first == x.g) {
-            std::swap(*t, *t2);  ++t2;
-         } else if (t2->first == x.g) {
-            ++t2;
-         } else {
-            if (!Logger_dummy) {
-               negate(x.k1);
-               Logger.from_left(SparseMatrix2x2<E>(M.col(t->second).begin().index(), M.col(t2->second).begin().index(),
-                                                   x.k2, x.p*x.k1,
-                                                   1,    x.q) );
-               Logger.from_right(SparseMatrix2x2<E>(t->second, t2->second,
-                                                    x.q*x.k2, x.p,
-                                                    x.k1,     1) );
-            }
-            t->first *= x.k2;
-            if (strict_diagonal) {
-               M.col(t->second).front()*=x.k2;
-               M.col(t2->second).front()=x.g;
-            }
-            if (is_one(x.g)) {
-               if (strict_diagonal) *rp++=M.col(t2->second).begin().index(), *cp++=t2->second;
-               torsion.erase(t2++);
-            } else {
-               t2->first=x.g;  ++t2;
-            }
-         }
-      }
-   }
-
-   if (strict_diagonal) {
-      for (typename Entire<std::list< std::pair<E,int> > >::reverse_iterator t=rentire(torsion);  !t.at_end();  ++t)
-         *rp++=M.col(t->second).begin().index(), *cp++=t->second;
-
-      if (rp < rpe)
-         for (typename Rows< SparseMatrix<E> >::iterator r=rows(M).begin(); ; ++r)
-            if (r->empty()) {
-               *rp++=r.index();
-               if (rp==rpe) break;
-            }
-      if (cp < cpe)
-         for (typename Cols< SparseMatrix<E> >::iterator c=cols(M).begin(); ; ++c)
-            if (c->empty()) {
-               *cp++=c.index();
-               if (cp==cpe) break;
-            }
-
-      Logger.permute_rows(r_perm);
-      Logger.permute_cols(c_perm);
-      M.permute_rows(entire(r_perm));
-      M.permute_cols(entire(c_perm));
-   }
 
    return rank;
 }
 
-template <typename E>
-void compress_torsion(std::list< std::pair<E,int> >& torsion)
-{
-   for (typename std::list< std::pair<E,int> >::iterator t=torsion.begin(), t_end=torsion.end(); t != t_end; ++t) {
-      t->second=1;
-      typename std::list< std::pair<E,int> >::iterator t2=t;  ++t2;
-      for (;;) {
-         if (t2 == t_end) return;
-         if (t->first == t2->first) {
-            ++t->second;
-            t2=torsion.erase(t2);
-         } else {
-            break;
-         }
-      }
-   }
-}
-
-template <typename E, typename CompanionLogger>
-int eliminate_ones(SparseMatrix<E>& M, Bitset& elim_rows, Bitset& elim_cols, const CompanionLogger& Logger)
-{
-   const bool Logger_dummy=pm::derived_from<CompanionLogger, dummy_companion_logger>::value;
-
-#if POLYMAKE_DEBUG
-   const bool do_dump=getenv("SMITH_DUMP");
-   if (do_dump) cout << "eliminate_ones(initial):\n" << std::setw(6) << M;
-   pm::SparseMatrixStatistics<E> stat;
-   stat.gather(M);
-   cout << "eliminate_ones(initial statistics):\n" << stat;
-#endif
-
-   SparseMatrix2x2<E> U;
-   const int R=M.rows(), C=M.cols();
-   elim_rows.clear(); elim_rows.reserve(R);
-   elim_cols.clear(); elim_cols.reserve(C);
-   int count=0;
-
-   for (int c=0, c_last=C;  c != c_last;  c==C && c_last<C && (c=0)) {    // repeat until matrix is empty or contains no +1/-1 more
-      if (M.col(c).empty() || elim_cols.contains(c)) { ++c; continue; }
-
-      typename SparseMatrix<E>::col_type::iterator e=M.col(c).begin();
-      while (!abs_equal(*e,1) && !(++e).at_end()) ;
-      if (e.at_end()) { ++c; continue; }
-
-      int r=e.index();
-
-      if (!Logger_dummy) {
-         U.i=r; U.a_ii=one_value<E>(); U.a_jj=one_value<E>(); U.a_ij=zero_value<E>();
-      }
-      for (typename SparseMatrix<E>::col_type::iterator e2=M.col(c).begin(); !e2.at_end(); ) {
-         if ((U.j=e2.index())==r) { ++e2; continue; }
-         if (abs_equal(*e,*e2)) {
-            if (pm::sign(*e)==pm::sign(*e2)) {
-               if (!Logger_dummy) U.a_ji=-one_value<E>();
-               ++e2;
-               M.row(U.j) -= M.row(r);
-            } else {
-               if (!Logger_dummy) U.a_ji=one_value<E>();
-               ++e2;
-               M.row(U.j) += M.row(r);
-            }
-         } else {
-            U.a_ji= (*e)>0 ? -(*e2) : (*e2);
-            ++e2;
-            M.row(U.j) += U.a_ji * M.row(r);
-         }
-         Logger.from_left(U);
-      }
-      if (Logger_dummy) {
-         M.row(r).clear();
-         ++count;
-      } else {
-         U.i=c; U.a_ji=zero_value<E>();
-         for (typename SparseMatrix<E>::row_type::iterator e2=M.row(r).begin(); !e2.at_end(); ) {
-            if ((U.j=e2.index())==c) { ++e2; continue; }
-            U.a_ij= (*e)>0 ? -(*e2) : (*e2);
-            Logger.from_right(U);
-            M.row(r).erase(e2++);
-         }
-      }
-      elim_rows+=r;  elim_cols+=c;
-#if POLYMAKE_DEBUG
-      stat.gather(M);
-      if (do_dump) cout << "eliminate_ones(eliminating col " << c << " [" << r << "] ):\n" << std::setw(6) << M;
-#endif
-      c_last=c++;
-   }
-
-#if POLYMAKE_DEBUG
-   cout << "eliminate_ones(final statistics):\n" << stat << endl;
-#endif
-   return count;
-}
 
 /// Compute the compact respresentation of the Hermite normal form, without companion matrices.
 /// Input matrix M is corrupted during the computations.
 /// @param[out] torsion list of diagonal elements of HNF not equal 1 with multiplicities.
 /// @return rank of M.
 template <typename E> inline
-int hermite_normal_form_only(SparseMatrix<E>& M, std::list< std::pair<E,int> >& torsion)
+int hermite_normal_form_only(SparseMatrix<E>& M )
 {
-   int rank=hermite_normal_form(M, torsion, dummy_companion_logger(), False());
-   compress_torsion(torsion);
+   int rank=hermite_normal_form(M, dummy_companion_logger(), False());
    return rank;
 }
 
@@ -381,12 +198,9 @@ class HermiteNormalForm :
    public GenericStruct<HermiteNormalForm<E> > {
 public:
    typedef SparseMatrix<E> matrix_type;
-   typedef std::list< std::pair<E,int> > torsion_type;
 
    DeclSTRUCT( DeclTemplFIELD(form, matrix_type)                // input matrix converted to the normal form
-               DeclTemplFIELD(left_companion, matrix_type)      // input = left_companion * form * right_companion  OR
                DeclTemplFIELD(right_companion, matrix_type)     // form = left_companion * input * right_companion (inverted call)
-               DeclTemplFIELD(torsion, torsion_type)            // list of torsion coefficients extracted from the diagonal of form
                DeclTemplFIELD(rank, int) );                     // rank of form
 };
 
@@ -400,23 +214,23 @@ hermite_normal_form(const GenericMatrix<Matrix, E>& M,
 {
    HermiteNormalForm<E> res;
    res.form=M;
-   res.left_companion=unit_matrix<E>(M.rows());
    res.right_companion=unit_matrix<E>(M.cols());
    if (inverse_companions)
-      res.rank=hermite_normal_form(res.form, res.torsion, HNF_companion_logger<E, false>(&res.left_companion, &res.right_companion), True());
+      res.rank=hermite_normal_form(res.form, HNF_companion_logger<E, false>(&res.right_companion), True());
    else
-      res.rank=hermite_normal_form(res.form, res.torsion, HNF_companion_logger<E, true>(&res.left_companion, &res.right_companion), True());
-   compress_torsion(res.torsion);
+      res.rank=hermite_normal_form(res.form, HNF_companion_logger<E, true>(&res.right_companion), True());
    return res;
 }
 
 }
-namespace polymake {
+}
+namespace polymake { namespace common {
 
 using pm::HermiteNormalForm;
 using pm::hermite_normal_form;
 using pm::hermite_normal_form_only;
 
+}
 }
 
 #endif // POLYMAKE_SMITH_NORMAL_FORM_H
