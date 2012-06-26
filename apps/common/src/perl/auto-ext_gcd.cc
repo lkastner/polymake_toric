9ac5282b97b050b18a3ef69295cc104a1479555d
lars <lars.kastner@yahoo.dk>
2012-06-26 10:47:57 +0200

    Adding deformations.rules + autogenerated stuff.

---
+++ apps/common/src/perl/auto-ext_gcd.cc
@@ -0,0 +1,31 @@
+/* Copyright (c) 1997-2010
+   Ewgenij Gawrilow, Michael Joswig (Technische Universitaet Darmstadt, Germany)
+   http://www.polymake.de
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version: http://www.gnu.org/licenses/gpl.txt.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+*/
+
+///==== this line controls the automatic file splitting: max.instances=40
+
+#include "polymake/client.h"
+#include "polymake/numerical_functions.h"
+#include "polymake/Integer.h"
+namespace polymake { namespace common {
+///==== Automatically generated contents follow.    Please do not delete this line. ====
+   template <typename T0, typename T1>
+   FunctionInterface4perl( ext_gcd_X_X, T0,T1 ) {
+      perl::Value arg0(stack[0]), arg1(stack[1]);
+      WrapperReturn( ext_gcd(arg0.get<T0>(), arg1.get<T1>()) );
+   };
+
+   FunctionInstance4perl(ext_gcd_X_X, perl::Canned< const Integer >, perl::Canned< const Integer >);
+///==== Automatically generated contents end here.  Please do not delete this line. ====
+} }
